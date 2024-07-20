a4d4198db3dfdd1d350df87473b12509515e61af
savasduzgun <savasdzgn@gmail.com>
2024-07-20 16:23:39 +0300

    Add project files.

---
+++ BlazorGiftSuggestionApp/Enums/RelationshipType.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorGiftSuggestionApp.Enums
+{
+    public enum RelationshipType
+    {
+
+        Friend = 1,
+        Relative = 2,
+        Parent = 3,
+        Brother = 4,
+        Sister = 5,
+        Child = 6,
+        Spouse = 7,
+        Colleague = 8,
+        Other = 9,
+
+    }
+}
